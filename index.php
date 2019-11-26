73b732a38812028e946ba127ef8bf4ec9445869f
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-11-26 23:04:45 +0700

    main code

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+class Gambar {
+    protected $src=__DIR__.'/assets';
+    protected $option=array();
+    protected $option2=array();
+    protected $option3=array();
+    protected $text;
+
+    function __construct($text='hajigur', $font='FSEX300.ttf', $query='random')
+    {
+        $this->option=[
+            'color'=>'white',
+            'size'=>100,
+            'anchor'=>'center',
+            'fontFile'=>$this->src.'/'.$font
+        ];
+        $this->option2=[
+            'color'=>'white',
+            'size'=>50,
+            'anchor'=>'bottom left',
+            'fontFile'=>$this->src.'/'.$font,
+            'xOffset'=>100
+        ];
+        $this->option3=[
+            'color'=>'white',
+            'size'=>50,
+            'anchor'=>'bottom left',
+            'xOffset'=>405,
+            'fontFile'=>$this->src.'/'.$font
+        ];
+            $this->text=$text;
+            $this->query=$query;
+            $this->font=$font;
+    }
+    private function filterParagraf($paragraf, $wrap)
+    {
+        $paragraf=trim($paragraf);
+        $paragraf=str_replace('. ', ".\n\n", $paragraf);
+        $paragraf=wordwrap($paragraf, $wrap, "\n");
+        $paragraf=ucwords(strtolower($paragraf));
+        return $paragraf;
+    }
+    public function getResult($result, $mime, $quality)
+    {
+        try {
+            $tele  = new claviska\SimpleImage();
+            $fb    = new claviska\SimpleImage();
+            $image = new claviska\SimpleImage();
+
+            $tele
+                ->fromFile($this->src.'/telegram.png')
+                ->resize(70,70);
+
+            $fb
+                ->fromFile($this->src.'/facebook.png')
+                ->resize(90,80);
+
+            $image
+                ->fromFile($this->src.'/bg.jpg')
+                ->resolution(320, 200)
+                ->autoOrient()
+                ->text(self::filterParagraf($this->text, 40), $this->option)
+                ->overlay($tele,'bottom left')
+                ->text('BHSec', $this->option2)
+                ->overlay($fb, 'bottom left',1,280)
+                ->text('BHSec', $this->option3) 
+                ->toFile($result, $mime, $quality);
+        }
+        catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+//    private function curlSendRequest($url){
+//        $curl = curl_init();
+//        curl_setopt($curl, CURLOPT_URL, $url);
+//        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array(
+//            'query' => $this->query,
+//            'per_page'=>50,
+//            'client_id'=>$this->key_unsplash
+//        )));
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+//        $result = curl_exec($curl);
+//        curl_close($curl);
+//        return json_decode($result, true);
+//    }
+}
+try {
+    $paragraf="bihun adalah bahasa pemrograman interpretatif multiguna dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.";
+    $draw=new Gambar(); //paragraf, font, query
+    $draw->getResult('result.png', 'image/png', 100); //result, mime, quality
+}catch(Exception $e) {
+    echo $e->getMessage()."\n";
+}
