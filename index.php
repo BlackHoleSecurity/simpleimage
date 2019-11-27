5f4aab4bf9e7668f3b63a34196837bce3e52b772
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-11-27 17:29:01 +0700

    add unsplash and json result

--- index.php
+++ index.php
@@ -1,55 +1,60 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-class Gambar {
-    protected $src=__DIR__.'/assets';
-    protected $option=array();
-    protected $option2=array();
-    protected $option3=array();
-    protected $text;
+namespace Simple;
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/Unsplash.php';
 
-    function __construct($text = 'hajigur', $font = 'FSEX300.ttf', $query = 'random')
+class Gambar
+{
+    protected $src = __DIR__ . '/assets';
+    protected $option = array();
+    protected $option2 = array();
+    protected $option3 = array();
+    protected $text;
+    protected $query;
+
+    function __construct($text = 'hajigur', $query = 'random', $font = 'FSEX300.ttf')
     {
-        $this->option=[ // main text
+        $this->option = [ // main text
             'color'    => 'white',
             'size'     => 100,
             'anchor'   => 'center',
-            'fontFile' => $this->src.'/'.$font,
-
-            'shadow'=>[ // shadow option
+            'fontFile' => $this->src . '/' . $font,
+            'shadow' => [ // shadow option
                 'x'     => 15,
                 'y'     => 15,
-                'color' =>'black'
+                'color' => 'black'
             ]
         ];
 
-        $this->option2=[ // bottom text 1
+        $this->option2 = [ // bottom text 1
             'color'    => 'white',
             'size'     => 50,
             'anchor'   => 'bottom left',
-            'fontFile' => $this->src.'/'.$font,
+            'fontFile' => $this->src . '/' . $font,
             'xOffset'  => 100
         ];
 
-        $this->option3=[ // bottom text 2
+        $this->option3 = [ // bottom text 2
             'color'    => 'white',
             'size'     => 50,
             'anchor'   => 'bottom left',
             'xOffset'  => 405,
-            'fontFile' => $this->src.'/'.$font
+            'fontFile' => $this->src . '/' . $font
         ];
 
-        $this->option4=[ // top text 1
+        $this->option4 = [ // top text 1
             'color'    => 'white',
             'size'     => 50,
             'anchor'   => 'top right',
             'xOffset'  => -300,
             'yOffset'  => 140,
-            'fontFile' => $this->src.'/'.$font
+            'fontFile' => $this->src . '/' . $font
         ];
 
         $this->text  = $text;
         $this->query = $query;
     }
+
     private function filterParagraf($paragraf, $wrap)
     {
         $paragraf = trim($paragraf);
@@ -58,63 +63,58 @@ class Gambar {
         $paragraf = ucwords(strtolower($paragraf));
         return $paragraf;
     }
+
     public function getResult($result, $mime, $quality)
     {
         try {
-            $tele  = new claviska\SimpleImage();
-            $fb    = new claviska\SimpleImage();
-            $bhs   = new claviska\SimpleImage();
-            $image = new claviska\SimpleImage();
+            $tele  = new \claviska\SimpleImage();
+            $fb    = new \claviska\SimpleImage();
+            $bhs   = new \claviska\SimpleImage();
+            $image = new \claviska\SimpleImage();
+            $unsplash = new \Unsplash($this->query);
 
             $tele
-                ->fromFile($this->src.'/telegram.png')
-                ->resize(70,70);
+                ->fromFile($this->src . '/telegram.png')
+                ->resize(70, 70);
 
             $fb
-                ->fromFile($this->src.'/facebook.png')
-                ->resize(90,80);
+                ->fromFile($this->src . '/facebook.png')
+                ->resize(90, 80);
 
             $bhs
-                ->fromFile($this->src.'/bhs.png')
-                ->resize(300,300);
+                ->fromFile($this->src . '/bhs.png')
+                ->resize(300, 300);
 
             $image
-                ->fromFile($this->src.'/bg.jpg')
+                ->fromString(file_get_contents($unsplash->result_regular))
                 ->resolution(320, 200)
+                ->resize(2016, 2016)
                 ->autoOrient()
                 ->text(self::filterParagraf($this->text, 40), $this->option)
-                ->overlay($tele,'bottom left')
+                ->overlay($tele, 'bottom left')
                 ->text('BHSec', $this->option2)
                 ->overlay($bhs, 'top right', 0.45)
                 ->text('Did you know?', $this->option4)
                 ->overlay($fb, 'bottom left', 1, 280)
                 ->text('BlackHoleSecurity', $this->option3)
                 ->toFile($result, $mime, $quality);
-        }
-        catch(Exception $e) {
+            $return = [
+                'Exif' => $image->getExif(),
+                'Orientation' => $image->getOrientation(),
+                'Resolution' => $image->getResolution(),
+                'AspectRatio' => $image->getAspectRatio()
+            ];
+            return json_encode($return);
+        } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
     }
-    //    private function curlSendRequest($url){
-    //        $curl = curl_init();
-    //        curl_setopt($curl, CURLOPT_URL, $url);
-    //        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array(
-    //            'query' => $this->query,
-    //            'per_page'=>50,
-    //            'client_id'=>$this->key_unsplash
-    //        )));
-    //        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    //        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    //        $result = curl_exec($curl);
-    //        curl_close($curl);
-    //        return json_decode($result, true);
-    //    }
 }
 try {
-    $paragraf="bihun adalah bahasa pemrograman interpretatif multiguna dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.";
-
-    $draw = new Gambar($paragraf); //paragraf, font, query
-    $draw->getResult('result.png', 'image/png', 100); //result, mime, quality
-} catch(Exception $e) {
-    echo $e->getMessage()."\n";
+    $paragraf = "bihun adalah bahasa pemrograman interpretatif multiguna dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.";
+    //$paragraf = readline('text : ');
+    $draw = new Gambar($paragraf, 'Dark'); //paragraf, font, query
+    echo $draw->getResult('result.png', 'image/png', 100) . "\n"; //result, mime, quality
+} catch (Exception $e) {
+    echo $e->getMessage() . "\n";
 }
