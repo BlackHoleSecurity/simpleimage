141397e0ae9535f11c017cd6627858212883f98a
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-11-29 16:29:50 +0700

    removed

---
+++ src/Gambar.php
@@ -0,0 +1,112 @@
+<?php
+namespace Bhsec\SimpleImage;
+class Gambar
+{
+    protected $src     = __DIR__ . '/assets';
+    protected $option  = array();
+    protected $option2 = array();
+    protected $option3 = array();
+    protected $option4 = array();
+    protected $text;
+    protected $query;
+
+    function __construct($text = 'hajigur', $query = 'random', $font = 'FSEX300.ttf')
+    {
+        $this->option = [ // main text
+            'color'    => 'white',
+            'size'     => 100,
+            'anchor'   => 'center',
+            'fontFile' => $this->src . '/' . $font,
+            'shadow' => [ // shadow option
+                'x'     => 15,
+                'y'     => 15,
+                'color' => 'black'
+            ]
+        ];
+
+        $this->option2 = [ // bottom text 1
+            'color'    => 'white',
+            'size'     => 50,
+            'anchor'   => 'bottom left',
+            'fontFile' => $this->src . '/' . $font,
+            'xOffset'  => 100
+        ];
+
+        $this->option3 = [ // bottom text 2
+            'color'    => 'white',
+            'size'     => 50,
+            'anchor'   => 'bottom left',
+            'xOffset'  => 405,
+            'fontFile' => $this->src . '/' . $font
+        ];
+
+        $this->option4 = [ // top text 1
+            'color'    => 'white',
+            'size'     => 50,
+            'anchor'   => 'top right',
+            'xOffset'  => -300,
+            'yOffset'  => 140,
+            'fontFile' => $this->src . '/' . $font
+        ];
+
+        $this->text  = $text;
+        $this->query = $query;
+    }
+
+    private function filterParagraf($paragraf, $wrap)
+    {
+        $paragraf = trim($paragraf);
+        $paragraf = str_replace('. ', ".\n\n", $paragraf);
+        $paragraf = wordwrap($paragraf, $wrap, "\n");
+        $paragraf = ucwords(strtolower($paragraf));
+        return $paragraf;
+    }
+
+    public function getResult($result, $mime, $quality)
+    {
+        try {
+            $tele     = new \claviska\SimpleImage();
+            $fb       = new \claviska\SimpleImage();
+            $bhs      = new \claviska\SimpleImage();
+            $image    = new \claviska\SimpleImage();
+            $unsplash = new \Bhsec\SimpleImage\Unsplash($this->query);
+
+            $tele
+                ->fromFile($this->src . '/telegram.png')
+                ->resize(70, 70);
+
+            $fb
+                ->fromFile($this->src . '/facebook.png')
+                ->resize(90, 80);
+
+            $bhs
+                ->fromFile($this->src . '/bhs.png')
+                ->resize(300, 300);
+
+            $image
+                ->fromString(file_get_contents($unsplash->result_regular))
+                ->resolution(320, 200)
+                ->resize(2016, 2016)
+                ->autoOrient()
+                ->text(self::filterParagraf($this->text, 40), $this->option)
+                ->overlay($tele, 'bottom left')
+                ->text('BHSec', $this->option2)
+                ->overlay($bhs, 'top right', 0.45)
+                ->text('Did you know?', $this->option4)
+                ->overlay($fb, 'bottom left', 1, 280)
+                ->text('BlackHoleSecurity', $this->option3)
+                ->toFile($result, $mime, $quality);
+
+            $return = [
+                'Exif' => $image->getExif(),
+                'Orientation' => $image->getOrientation(),
+                'Resolution'  => $image->getResolution(),
+                'AspectRatio' => $image->getAspectRatio()
+            ];
+
+            return json_encode($return);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
---
+++ src/Unsplash.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bhsec\SimpleImage;
+class Unsplash
+{
+    protected static $access_unsplash = '9e33a4b9f59c0dbb71800d9eae09377d70c0de27c815b73a508f5f35dd6494ed';
+    protected static $secret_unsplash = '56632e0c65b061c0235b1d4be6afe3e3ff100b4d0c082e81066bd986ea53ec8d';
+    protected static $page = 15;
+    protected static $per_page = 15;
+    protected static $orientation = 'landscape';// 'portrait';
+    public $result_full;
+    public $result_regular;
+    public $result_small;
+    public $result_thumb;
+    public $result_raw;
+
+    function __construct($query)
+    {
+        \Crew\Unsplash\HttpClient::init(
+            [
+                'applicationId' => self::$access_unsplash,
+                'secret' => self::$secret_unsplash,
+                'utmSource' => 'simpleimage'
+            ]
+        );
+
+        $result = \Crew\Unsplash\Search::photos($query, self::$page, self::$per_page, self::$orientation);
+        $index=rand(0, 14);
+        $this->result_full = $result[$index]['urls']['full'];
+        $this->result_regular = $result[$index]['urls']['regular'];
+        $this->result_small = $result[$index]['urls']['small'];
+        $this->result_thumb = $result[$index]['urls']['thumb'];
+        $this->result_raw = $result[$index]['urls']['raw'];
+    }
+}
