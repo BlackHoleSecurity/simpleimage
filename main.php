d20907311f9b15249b15546883d8524736ccb384
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-05-09 10:53:39 +0800

    updates

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Bhsec\SimpleImage\Gambar;
+use Bhsec\SimpleImage\Unsplash;
+
+$paragraf = "bihun adalah bahasa pemrograman interpretatif multiguna dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.";
+$gambar = new Gambar($paragraf, 'Blur');
+$result = $gambar->getResult('result.jpg', 'image/jpeg', 10); // filename, mime, quality
+echo $result;
