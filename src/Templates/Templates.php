64c4f7bb0efd8b0d535177b815d1e1e45d17aaee
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-05-27 08:22:55 +0800

    container

--- src/Templates/Templates.php
+++ src/Templates/Templates.php
@@ -11,12 +11,50 @@ abstract class Templates
         $container['image'] = $container->factory(function () {
             return new \claviska\SimpleImage();
         });
+
+        $container['query'] = 'dark';
         $container['unsplash'] = function ($c) {
             return new \Bhsec\SimpleImage\Unsplash($c['query']);
         };
+
+        $container['client'] = function()
+        {
+            return new \GuzzleHttp\Client();
+        };
+
+        $container['unsplash_regular'] = function($c)
+        {
+            $response = $c['client']->request('GET', $c['unsplash']->regular);
+            return $response->getBody();
+        };
+
+        $container['unsplash_small'] = function($c)
+        {
+            $response = $c['client']->request('GET', $c['unsplash']->small);
+            return $response->getBody();
+        };
+
+        $container['unsplash_thumb'] = function($c)
+        {
+            $response = $c['client']->request('GET', $c['unsplash']->thumb);
+            return $response->getBody();
+        };
+
+        $container['unsplash_raw'] = function($c)
+        {
+            $response = $c['client']->request('GET', $c['unsplash']->raw);
+            return $response->getBody();
+        };
+
+        $container['unsplash_full'] = function($c)
+        {
+            $response = $c['client']->request('GET', $c['unsplash']->full);
+            return $response->getBody();
+        };
+
         return $container;
     }
-    public static function filterParagraf(string $paragraf, string $wrap)
+    public static function filterParagraf(string $paragraf, int $wrap): string
     {
         $paragraf = trim($paragraf);
         $paragraf = mb_substr($paragraf, 0, 301, 'UTF-8');
