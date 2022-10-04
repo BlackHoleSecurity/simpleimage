78b8766b0c4fe50b6d686c6ab19033e4b46af6f3
Cvar1984 <Cvar1984@pm.me>
2022-10-04 11:55:22 +0700

    env vars

--- src/Unsplash.php
+++ src/Unsplash.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace Bhsec\SimpleImage;
 
 class Unsplash
 {
-    protected const ACCESS_UNSPLASH = '9e33a4b9f59c0dbb71800d9eae09377d70c0de27c815b73a508f5f35dd6494ed';
-    protected const SECRET_UNSPLASH = '56632e0c65b061c0235b1d4be6afe3e3ff100b4d0c082e81066bd986ea53ec8d';
+    private $unsplashAccess; 
+    private $unsplashSecret;
     protected static $per_page = 15;
     protected static $orientation = 'landscape'; // 'portrait';
 
@@ -16,9 +17,13 @@ class Unsplash
 
     public function __construct($query)
     {
+	\Dotenv\Dotenv::createImmutable(__DIR__. '/..')->safeLoad();
+	$this->unsplashAccess = $_ENV['ACCESS_UNSPLASH'];
+	$this->unsplashSecret = $_ENV['SECRET_UNSPLASH'];
+
         \Crew\Unsplash\HttpClient::init([
-            'applicationId' => Unsplash::ACCESS_UNSPLASH,
-            'secret' => Unsplash::SECRET_UNSPLASH,
+            'applicationId' => $this->unsplashAccess,
+            'secret' => $this->unsplashSecret,
             'utmSource' => 'simpleimage',
         ]);
 
